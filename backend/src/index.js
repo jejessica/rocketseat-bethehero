c6cfe76a3e24ca6c11a4cca8d2b73d2c8a20c9cb
Jessica <jmd.sants@gmail.com>
2020-03-26 00:23:23 -0300

    alteração index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const cors = require('cors')
-const routes  = require("./routes")
+const express = require('express');
+const cors = require('cors');
+const routes  = require("./routes");
 
-const app = express()
+const app = express();
 
 app.use(cors())
 app.use(express.json())
